ae80ff8a0f2b226e628b798e2016803a93a19ff4
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-04-18 01:29:06 +1000

    used for verify if there is a required access

---
+++ interviewgoose-frontend/src/access/verifyAccess.ts
@@ -0,0 +1,36 @@
+import ACCESS_ENUM from "@/access/accessEnum";
+
+/**
+ * verify if the logged user have a specific access
+ * @param loginUser
+ * @param needAccess
+ * @return boolean
+ */
+const verifyAccess = (
+  loginUser: API.LoginUserVO,
+  needAccess = ACCESS_ENUM.NOT_LOGIN,
+) => {
+  // Check what access the currently logged user have
+  // no access, by default, if there is no logged user
+  const loginUserAccess = loginUser?.userRole ?? ACCESS_ENUM.NOT_LOGIN;
+  // need no any access
+  if (needAccess === ACCESS_ENUM.NOT_LOGIN) {
+    return true;
+  }
+  // need to log in to access
+  if (needAccess === ACCESS_ENUM.USER) {
+    // no access if user not logged in
+    if (needAccess === ACCESS_ENUM.NOT_LOGIN) {
+      return false;
+    }
+  }
+  // need to be admin to access
+  if (needAccess === ACCESS_ENUM.ADMIN) {
+    if (needAccess !== ACCESS_ENUM.ADMIN) {
+      return false;
+    }
+    return true;
+  }
+};
+
+export default verifyAccess;
