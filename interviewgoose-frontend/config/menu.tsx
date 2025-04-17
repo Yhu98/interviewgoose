29e0f37fb6e548e22018382e4bea7160e5199528
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-04-18 02:11:12 +1000

    access control in menu configuration; helper functions to find a menu item by its path

--- interviewgoose-frontend/config/menu.tsx
+++ interviewgoose-frontend/config/menu.tsx
@@ -1,30 +1,56 @@
-import { MenuDataItem } from "@ant-design/pro-layout";
-import { CrownOutlined } from "@ant-design/icons";
+import {MenuDataItem} from "@ant-design/pro-layout";
+import {CrownOutlined} from "@ant-design/icons";
+import ACCESS_ENUM from "@/access/accessEnum";
 
 const menus = [
-  {
-    path: "/",
-    name: "Home",
-  },
-  {
-    path: "/questions",
-    name: "Questions",
-  },
-  {
-    path: "/topics",
-    name: "Topics",
-  },
-  {
-    path: "/admin",
-    name: "Administration",
-    icon: <CrownOutlined />,
-    children: [
-      {
-        path: "/admin/user",
-        name: "User Administration",
-      },
-    ],
-  },
+    {
+        path: "/",
+        name: "Home",
+    },
+    {
+        path: "/questions",
+        name: "Questions",
+    },
+    {
+        path: "/topics",
+        name: "Topics",
+    },
+    {
+        path: "/admin",
+        name: "Administration",
+        icon: <CrownOutlined/>,
+        access: ACCESS_ENUM.ADMIN,
+        children: [
+            {
+                path: "/admin/user",
+                name: "User Administration",
+                access: ACCESS_ENUM.ADMIN,
+            },
+        ],
+    },
 ] as MenuDataItem[];
+
+//
+export const findAllMenuItemByPath = (path: string): MenuDataItem | null => {
+    return findMenuItemByPath(menus, path);
+}
+
+export const findMenuItemByPath = (
+    menus: MenuDataItem[],
+    path: string
+): MenuDataItem | null => {
+    for (const menu of menus) {
+        if (menu.path === path) {
+            return menu;
+        }
+        if (menu.children) {
+            const matchedMenuItem: MenuDataItem | null = findMenuItemByPath(menu.children, path);
+            if (matchedMenuItem) {
+                return matchedMenuItem;
+            }
+        }
+    }
+    return null;
+}
 
 export default menus;
