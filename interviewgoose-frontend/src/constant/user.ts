8feca0e4196adda107b7ee7aa0a5398839ebf905
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-04-19 00:39:18 +1000

    minor fix

--- interviewgoose-frontend/src/constant/user.ts
+++ interviewgoose-frontend/src/constant/user.ts
@@ -2,7 +2,7 @@ import ACCESS_ENUM from "@/access/accessEnum";
 
 // Default User
 export const DEFAULT_USER: API.LoginUserVO = {
-    userName: "Not logged in",
+    userName: "Sign in",
     userProfile: "Nothing to say here ......",
     userAvatar: "/assets/notLoginUser.png",
     userRole: ACCESS_ENUM.NOT_LOGIN,
