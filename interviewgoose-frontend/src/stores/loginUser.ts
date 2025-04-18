d2b5ab6ad53674b4db2d4d8b8085ef1b2f4d0429
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-04-18 20:57:57 +1000

    move DEFAULT_USER to constants and update import

--- interviewgoose-frontend/src/stores/loginUser.ts
+++ interviewgoose-frontend/src/stores/loginUser.ts
@@ -1,30 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import ACCESS_ENUM from "@/access/accessEnum";
-
-// Default User
-const DEFAULT_USER: API.LoginUserVO = {
-    userName: "No user logged in",
-    userProfile: "Nothing to say here ......",
-    userAvatar: "/assets/notLoginUser.png",
-    userRole: ACCESS_ENUM.NOT_LOGIN,
-};
+import { DEFAULT_USER } from "@/constant/user";
 
 /**
  * Logged in User Global State
  */
 export const loginUserSlice = createSlice({
-    name: "loginUser",
-    initialState: DEFAULT_USER,
-    reducers: {
-        setLoginUser: (state, action: PayloadAction<API.LoginUserVO>) => {
-            return {
-                ...action.payload,
-            };
-        },
+  name: "loginUser",
+  initialState: DEFAULT_USER,
+  reducers: {
+    setLoginUser: (state, action: PayloadAction<API.LoginUserVO>) => {
+      return {
+        ...action.payload,
+      };
     },
+  },
 });
 
 // modify the state
 export const { setLoginUser } = loginUserSlice.actions;
-
 export default loginUserSlice.reducer;
