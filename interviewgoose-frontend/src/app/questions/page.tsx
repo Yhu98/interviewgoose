34e3144a6f963a234e6acb8ece04dabe396786c3
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-07 22:57:50 +1000

    all question page

---
+++ interviewgoose-frontend/src/app/questions/page.tsx
@@ -0,0 +1,37 @@
+import Title from "antd/es/typography/Title";
+import { listQuestionVoByPageUsingPost } from "@/api/questionController";
+import "./index.css";
+import QuestionTable from "@/components/QuestionTable/page";
+
+/**
+ * Question Page
+ * @constructor
+ */
+export default async function QuestionsPage({ searchParams }) {
+  const {q: searchText} = searchParams;
+  let questionList = [];
+  let total = 0;
+
+  try {
+    const res = await listQuestionVoByPageUsingPost({
+      title: searchText,
+      pageSize: 12,
+      sortField: "createTime",
+      sortOrder: "descend",
+    });
+    questionList = res.data.records ?? [];
+    total = questionRes.data.total ?? 0;
+  } catch (e) {
+    // @ts-ignore
+    console.error("Couldn't find questions，" + e.message);
+  }
+
+  return (
+      <div id="questionsPage" className="max-width-content">
+        <Title level={3}>All Questions</Title>
+        <QuestionTable defaultQuestionList={questionList} defultTotal={total} defaultSearchParams={{
+          title: searchText,
+        }} />
+      </div>
+  );
+}
