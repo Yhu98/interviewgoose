888a6928887da011296986a2e29a7376c33a23c3
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-08 04:24:34 +1000

    feat: specific question bank (topic) page

---
+++ interviewgoose-frontend/src/app/bank/[questionBankId]/page.tsx
@@ -0,0 +1,70 @@
+"use server";
+import "./index.css";
+import Title from "antd/es/typography/Title";
+import { Avatar, Button, Card } from "antd";
+import { getQuestionBankVoByIdUsingGet } from "@/api/questionBankController";
+import QuestionList from "@/components/QuestionList";
+import Meta from "antd/es/card/Meta";
+import Paragraph from "antd/es/typography/Paragraph";
+
+/**
+ * Home Page
+ * @constructor
+ */
+export default async function BankPage({ params }) {
+  const { questionBankId } = params;
+  let bank = undefined;
+  try {
+    const res = await getQuestionBankVoByIdUsingGet({
+      id: questionBankId,
+      needQueryQuestionList: true,
+      pageSize: 200,
+    });
+    bank = res.data;
+  } catch (e) {
+    // @ts-ignore
+    console.error("Couldn't fetch topic. " + e.message);
+  }
+  if (!bank) {
+    return <div>Topic not found</div>;
+  }
+  let firstQuestionId;
+  if (bank.questionPage?.records && bank.questionPage.records.length > 0) {
+    firstQuestionId = bank.questionPage.records[0];
+  }
+
+  return (
+    <div id="bankPage" className="max-width-content">
+      <Card>
+        <Meta
+          avatar={<Avatar src={bank.picture} size={72} />}
+          title={
+            <Title level={3} style={{ marginBottom: 0 }}>
+              {bank.title}
+            </Title>
+          }
+          description={
+            <>
+              <Paragraph type="secondary">{bank.description}</Paragraph>
+              <Button
+                type="primary"
+                shape="round"
+                href={`/bank/${questionBankId}/question/${firstQuestionId}`}
+                target="_blank"
+                disabled={!firstQuestionId}
+              >
+                Start Answering
+              </Button>
+            </>
+          }
+        />
+      </Card>
+      <div style={{ marginBottom: 16 }} />
+      <QuestionList
+        questionBankId={questionBankId}
+        questionList={bank.questionPage?.records || []}
+        cardTitle={`Question List（${bank.questionPage?.total || 0}）`}
+      />
+    </div>
+  );
+}
