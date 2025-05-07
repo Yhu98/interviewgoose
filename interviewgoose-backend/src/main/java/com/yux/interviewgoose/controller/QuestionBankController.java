fad1297a504b7f4fcbc08df14b28f7d2fde57fd9
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-07 18:45:21 +1000

    Allow data scraping size to 200

--- interviewgoose-backend/src/main/java/com/yux/interviewgoose/controller/QuestionBankController.java
+++ interviewgoose-backend/src/main/java/com/yux/interviewgoose/controller/QuestionBankController.java
@@ -188,8 +188,9 @@ public class QuestionBankController {
                                                                HttpServletRequest request) {
         long current = questionBankQueryRequest.getCurrent();
         long size = questionBankQueryRequest.getPageSize();
-        // 限制爬虫
-        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        // Limit spider
+        // Allow data scraping size to 200
+        ThrowUtils.throwIf(size > 200, ErrorCode.PARAMS_ERROR);
         // 查询数据库
         Page<QuestionBank> questionBankPage = questionBankService.page(new Page<>(current, size),
                 questionBankService.getQueryWrapper(questionBankQueryRequest));
