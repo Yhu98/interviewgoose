3dd78a797092dfcd0b5c52d81c151ffd111fd98a
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-14 15:08:18 +1000

    feat: component for updating topic that a question belongs to

---
+++ interviewgoose-frontend/src/app/admin/question/components/UpdateBankModal.tsx
@@ -0,0 +1,125 @@
+import { Form, message, Modal, Select } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  addQuestionBankQuestionUsingPost,
+  listQuestionBankQuestionVoByPageUsingPost,
+  removeQuestionBankQuestionUsingPost,
+} from "@/api/questionBankQuestionController";
+import { listQuestionBankVoByPageUsingPost } from "@/api/questionBankController";
+
+interface Props {
+  questionId?: number;
+  visible: boolean;
+  onCancel: () => void;
+}
+
+/**
+ * Update Popup (Related Topics for Question)
+ * @param props
+ * @constructor
+ */
+const UpdateBankModal: React.FC<Props> = (props) => {
+  const { questionId, visible, onCancel } = props;
+  const [form] = Form.useForm();
+  const [questionBankList, setQuestionBankList] = useState<
+      API.QuestionBankVO[]
+  >([]);
+
+  // 获取所属题库列表
+  const getCurrentQuestionBankIdList = async () => {
+    try {
+      const res = await listQuestionBankQuestionVoByPageUsingPost({
+        questionId,
+        pageSize: 20,
+      });
+      const list = (res.data?.records ?? []).map((item) => item.questionBankId);
+      console.log(list);
+      form.setFieldValue("questionBankIdList" as any, list);
+    } catch (e) {
+      message.error("Couldn't get related topics list" + e.message);
+    }
+  };
+
+  useEffect(() => {
+    if (questionId) {
+      getCurrentQuestionBankIdList();
+    }
+  }, [questionId]);
+
+  // 获取题库列表
+  const getQuestionBankList = async () => {
+    // 题库数量不多，直接全量获取
+    const pageSize = 200;
+
+    try {
+      const res = await listQuestionBankVoByPageUsingPost({
+        pageSize,
+        sortField: "createTime",
+        sortOrder: "descend",
+      });
+      setQuestionBankList(res.data?.records ?? []);
+    } catch (e) {
+      message.error("Couldn't get topics list，" + e.message);
+    }
+  };
+
+  useEffect(() => {
+    getQuestionBankList();
+  }, []);
+
+  return (
+      <Modal
+          destroyOnClose
+          title={"Update Related Topic"}
+          open={visible}
+          footer={null}
+          onCancel={() => {
+            onCancel?.();
+          }}
+      >
+        <Form form={form} style={{ marginTop: 24 }}>
+          <Form.Item label="Related Topiics" name="questionBankIdList">
+            <Select
+                mode="multiple"
+                style={{ width: "100%" }}
+                options={questionBankList.map((questionBank) => {
+                  return {
+                    label: questionBank.title,
+                    value: questionBank.id,
+                  };
+                })}
+                onSelect={async (value) => {
+                  const hide = message.loading("Updating >>>");
+                  try {
+                    await addQuestionBankQuestionUsingPost({
+                      questionId,
+                      questionBankId: value,
+                    });
+                    hide();
+                    message.success("Successfully added to the topic");
+                  } catch (error: any) {
+                    hide();
+                    message.error("cc. " + error.message);
+                  }
+                }}
+                onDeselect={async (value) => {
+                  const hide = message.loading("Updating >>>");
+                  try {
+                    await removeQuestionBankQuestionUsingPost({
+                      questionId,
+                      questionBankId: value,
+                    });
+                    hide();
+                    message.success("Successfully removed frm the topic");
+                  } catch (error: any) {
+                    hide();
+                    message.error("Successfully add it to the topic，" + error.message);
+                  }
+                }}
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+  );
+};
+export default UpdateBankModal;
