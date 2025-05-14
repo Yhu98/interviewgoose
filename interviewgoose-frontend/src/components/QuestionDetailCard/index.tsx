d93c5527d8c5431c594b158e12f56f10ccfd1832
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-14 15:42:19 +1000

    feat: question detail card component

---
+++ interviewgoose-frontend/src/components/QuestionDetailCard/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Avatar, Card, List, Typography } from "antd";
+import "./index.css";
+import Link from "next/link";
+import TagList from "@/components/TagList";
+import MdViewer from "@/components/MdViewer";
+import Title from "antd/es/typography/Title";
+
+interface Props {
+  question: API.QuestionVO;
+}
+
+/**
+ * Question Bank (Topics) List Component
+ * @param props
+ * @constructor
+ */
+const QuestionDetailCard = (props: Props) => {
+  const { question } = props;
+
+  return (
+    <div className="question-detail-card">
+      <Card>
+        <Title level={1}>{question.title}</Title>
+        <TagList tagList={question.tagList} />
+        <div style={{ marginBottom: 16 }} />
+        <MdViewer value={question.content} />
+      </Card>
+      <div style={{ marginBottom: 16 }} />
+      <Card title="Recommended Answer">
+        <MdViewer value={question.answer} />
+      </Card>
+    </div>
+  );
+};
+
+export default QuestionDetailCard;
