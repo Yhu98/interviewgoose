37cd04e3b1a00e137d6af3c74107bc7e2d7d3afe
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-16 12:39:59 +1000

    feat: user center page

---
+++ interviewgoose-frontend/src/app/user/center/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Avatar, Card, Col, Row } from "antd";
+import Meta from "antd/es/card/Meta";
+import Paragraph from "antd/es/typography/Paragraph";
+import Title from "antd/es/typography/Title";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores";
+import React, { useState } from "react";
+import "./index.css";
+import CalendarChart from "@/app/user/center/components/CalendarChart";
+
+/**
+ * User Center Page
+ * @constructor
+ */
+export default function UserCenterPage() {
+  // Get user login info
+  const loginUser = useSelector((state: RootState) => state.loginUser);
+  // new variable for reuse
+  const user = loginUser;
+  // controlling tab highlighted in menu list
+  const [activeTabKey, setActiveTabKey] = useState<string>("info");
+
+  return (
+    <div id="userCenterPage" className="max-width-content">
+      <Row gutter={[16, 16]} className="user-row">
+        <Col xs={24} md={4}>
+          <Card style={{ textAlign: "center" }}>
+            <Avatar src={user.userAvatar} size={72} />
+            <div style={{ marginBottom: 16 }} />
+            <Meta
+              title={
+                <Title level={4} style={{ marginBottom: 0 }}>
+                  {user.userName}
+                </Title>
+              }
+              description={
+                <>
+                  <Paragraph type="secondary">{user.userProfile}</Paragraph>
+                </>
+              }
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={20}>
+          <Card
+            tabList={[
+              {
+                key: "record",
+                label: "Browsing Records",
+              },
+              {
+                key: "others",
+                label: "Others",
+              },
+            ]}
+            activeTabKey={activeTabKey}
+            onTabChange={(key: string) => {
+              setActiveTabKey(key);
+            }}
+          >
+              {activeTabKey === "record" && <><CalendarChart /></>}
+              {activeTabKey === "others" && <>bbbbb</>}
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
