8232c3162a3939a405de834686e505c1b0866198
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-18 13:46:59 +1000

    feat: add method to retrieve questions updated after a specific time, including deleted records

--- interviewgoose-backend/src/main/java/com/yux/interviewgoose/mapper/QuestionMapper.java
+++ interviewgoose-backend/src/main/java/com/yux/interviewgoose/mapper/QuestionMapper.java
@@ -2,14 +2,24 @@ package com.yux.interviewgoose.mapper;
 
 import com.yux.interviewgoose.model.entity.Question;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Date;
+import java.util.List;
 
 /**
 * @author Yuxuan
-* @description 针对表【question(Question)】的数据库操作Mapper
+* @description Mapper for database operations on the table [question (Question)]
 * @createDate 2025-04-02 18:34:14
 * @Entity com.yux.interviewgoose.model.entity.Question
 */
 public interface QuestionMapper extends BaseMapper<Question> {
+
+    /**
+     * Search Question List (including deleted data)
+     */
+    @Select("select * from question where updateTime >= #{minUpdateTime}")
+    List<Question> listQuestionWithDelete(Date minUpdateTime);
 
 }
 
