682ee77ea2135db3d46bd53c184c29ee7a21a149
Yhu98 <yuxuan.hu.1814@outlook.com>
2025-05-18 13:50:55 +1000

    feat: implement scheduled job to incrementally sync questions to Elasticsearch

---
+++ interviewgoose-backend/src/main/java/com/yux/interviewgoose/job/cycle/IncSyncQuestionToEs.java
@@ -0,0 +1,57 @@
+package com.yux.interviewgoose.job.cycle;
+
+import cn.hutool.core.collection.CollUtil;
+import com.yux.interviewgoose.esdao.QuestionEsDao;
+import com.yux.interviewgoose.mapper.QuestionMapper;
+import com.yux.interviewgoose.model.dto.question.QuestionEsDTO;
+import com.yux.interviewgoose.model.entity.Question;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Sync New Questions to es
+ *
+ * @author Hu
+ */
+// todo uncomment to start the job
+//@Component
+@Slf4j
+public class IncSyncQuestionToEs {
+
+    @Resource
+    private QuestionMapper questionMapper;
+
+    @Resource
+    private QuestionEsDao questionEsDao;
+
+    /**
+     * Execute per minute
+     */
+    @Scheduled(fixedRate = 60 * 1000)
+    public void run() {
+        // Query data in 5 minutes
+        Date fiveMinutesAgoDate = new Date(new Date().getTime() - 5 * 60 * 1000L);
+        List<Question> questionList = questionMapper.listQuestionWithDelete(fiveMinutesAgoDate);
+        if (CollUtil.isEmpty(questionList)) {
+            log.info("no inc question");
+            return;
+        }
+        List<QuestionEsDTO> questionEsDTOList = questionList.stream()
+                .map(QuestionEsDTO::objToDto)
+                .collect(Collectors.toList());
+        final int pageSize = 500;
+        int total = questionEsDTOList.size();
+        log.info("IncSyncQuestionToEs start, total {}", total);
+        for (int i = 0; i < total; i += pageSize) {
+            int end = Math.min(i + pageSize, total);
+            log.info("sync from {} to {}", i, end);
+            questionEsDao.saveAll(questionEsDTOList.subList(i, end));
+        }
+        log.info("IncSyncQuestionToEs end, total {}", total);
+    }
+}
